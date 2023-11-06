df57dfac2e416ed58f8dc03300625ded573e744c
Anton Laptop <magicdude4@gmail.com>
2023-11-06 14:49:38 +0200

    first commit

---
+++ base/urls.py
@@ -0,0 +1,15 @@
+# appliactionnnnnnnnnnnnnnnnnnnnnnnnnnnn
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('register', views.register),
+    path('members', views.member_only ),
+    path('login', views.MyTokenObtainPairView.as_view()),
+    path('products', views.Product_view.as_view()),
+    path('categories', views.Category_view.as_view()),
+]
+
+
+
+
